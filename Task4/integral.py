96364f8d6859df8541d8d73e79e7aca082bb8355
D. Chatani <dchatani@coma-140.vm.astro.rug.nl>
2026-03-04 22:46:19 +0100

    corrected 4.6

--- Task4/integral.py
+++ Task4/integral.py
@@ -1,15 +1,11 @@
-import scipy.integrate as integ
-
+import numpy as np
 func=input("Enter a function with symbols: ")
 anum = input("Enter lower bound: ")
 bnum = input("Enter upper bound: ")
 a = float(anum)
 b = float(bnum)
-
-
-from sympy import *
-init_printing(use_unicode=False)
-x = Symbol('x')
-answer = integrate(func, (x,a,b))
-
-print(f"The definite integral is: {answer}")
+N = 1000000  #increased N will increase accuracy. i chose 1000000
+x = np.linspace(a,b,N) #evenly spaced from a to b with N intervals
+y = np.sum(eval(func))
+integ = (b-a)/N*y
+print(integ)
